7b3c6e2d25f23b8791cc3d3e9df6998b96536024
Shubhangi <shubhangikgupta@gmail.com>
2017-11-29 19:19:05 +0530

    Photowala update

--- js/main.js
+++ js/main.js
@@ -17,67 +17,85 @@
 //            $('.portfolio-link').modal('hide');
 //        }
 //    });
-$('#portfolioModal1').on('show.bs.modal', function(e) {
-       window.location.hash = "modal";
-   console.log(window.location.href);
-   });
+// $('#portfolioModal1').on('show.bs.modal', function(e) {
+//        window.location.hash = "modal";
+//    console.log(window.location.href);
+//    });
+//
+//    $(window).on('hashchange', function (event) {
+//    console.log("Yo");
+//    console.log(window.location.href);
+//        if(window.location.hash != "#modal") {
+//            $('#portfolioModal1').modal('hide');
+//        }
+//    });
+//
+//    $('#portfolioModal2').on('show.bs.modal', function(e) {
+//           window.location.hash = "modal";
+//       console.log(window.location.href);
+//       });
+//
+//       $(window).on('hashchange', function (event) {
+//       console.log("Yo");
+//       console.log(window.location.href);
+//           if(window.location.hash != "#modal") {
+//               $('#portfolioModal2').modal('hide');
+//           }
+//       });
+//
+//       $('#portfolioModal3').on('show.bs.modal', function(e) {
+//              window.location.hash = "modal";
+//          console.log(window.location.href);
+//          });
+//
+//          $(window).on('hashchange', function (event) {
+//          console.log("Yo");
+//          console.log(window.location.href);
+//              if(window.location.hash != "#modal") {
+//                  $('#portfolioModal3').modal('hide');
+//              }
+//          });
+//
+//          $('#portfolioModal4').on('show.bs.modal', function(e) {
+//                 window.location.hash = "modal";
+//             console.log(window.location.href);
+//             });
+//
+//             $(window).on('hashchange', function (event) {
+//             console.log("Yo");
+//             console.log(window.location.href);
+//                 if(window.location.hash != "#modal") {
+//                     $('#portfolioModal4').modal('hide');
+//                 }
+//             });
+//
+//             $('#portfolioModal5').on('show.bs.modal', function(e) {
+//                    window.location.hash = "modal";
+//                console.log(window.location.href);
+//                });
+//
+//                $(window).on('hashchange', function (event) {
+//                console.log("Yo");
+//                console.log(window.location.href);
+//                    if(window.location.hash != "#modal") {
+//                        $('#portfolioModal5').modal('hide');
+//                    }
+//                });
+function clickProject(i) {
+	window.location.hash = "project"+i.toString();
+}
 
-   $(window).on('hashchange', function (event) {
-   console.log("Yo");
-   console.log(window.location.href);
-       if(window.location.hash != "#modal") {
-           $('#portfolioModal1').modal('hide');
-       }
-   });
+$('#portfolioModal1').on('show.bs.modal', function(e) { clickProject(this.id.slice(14)); });
+$('#portfolioModal2').on('show.bs.modal', function(e) { clickProject(this.id.slice(14)); });
+$('#portfolioModal3').on('show.bs.modal', function(e) { clickProject(this.id.slice(14)); });
+$('#portfolioModal4').on('show.bs.modal', function(e) { clickProject(this.id.slice(14)); });
+$('#portfolioModal5').on('show.bs.modal', function(e) { clickProject(this.id.slice(14)); });
+$('#portfolioModal6').on('show.bs.modal', function(e) { clickProject(this.id.slice(14)); });
 
-   $('#portfolioModal2').on('show.bs.modal', function(e) {
-          window.location.hash = "modal";
-      console.log(window.location.href);
-      });
-
-      $(window).on('hashchange', function (event) {
-      console.log("Yo");
-      console.log(window.location.href);
-          if(window.location.hash != "#modal") {
-              $('#portfolioModal2').modal('hide');
-          }
-      });
-
-      $('#portfolioModal3').on('show.bs.modal', function(e) {
-             window.location.hash = "modal";
-         console.log(window.location.href);
-         });
-
-         $(window).on('hashchange', function (event) {
-         console.log("Yo");
-         console.log(window.location.href);
-             if(window.location.hash != "#modal") {
-                 $('#portfolioModal3').modal('hide');
-             }
-         });
-
-         $('#portfolioModal4').on('show.bs.modal', function(e) {
-                window.location.hash = "modal";
-            console.log(window.location.href);
-            });
-
-            $(window).on('hashchange', function (event) {
-            console.log("Yo");
-            console.log(window.location.href);
-                if(window.location.hash != "#modal") {
-                    $('#portfolioModal4').modal('hide');
-                }
-            });
-
-            $('#portfolioModal5').on('show.bs.modal', function(e) {
-                   window.location.hash = "modal";
-               console.log(window.location.href);
-               });
-
-               $(window).on('hashchange', function (event) {
-               console.log("Yo");
-               console.log(window.location.href);
-                   if(window.location.hash != "#modal") {
-                       $('#portfolioModal5').modal('hide');
-                   }
-               });
+$(window).on('hashchange', function (event) {
+	for (i=1;i<=6;i++) {
+   		if(window.location.hash != "#project"+i.toString()) {
+            $('#portfolioModal'+i.toString()).modal('hide');
+        }
+	}
+});
